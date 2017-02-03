da1ce9f9afbfda0eed27c937ef496e06db0f7e7b
Gerhard Schlipf <gs_github@online.de>
2017-02-03 14:24:13 +0100

    Easy Change in MfA.java

--- MyFirstApp/src/main/MfA.java
+++ MyFirstApp/src/main/MfA.java
@@ -15,6 +15,7 @@ public class MfA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first Aplication in Github");
+		System.out.println("This is my first Aplication in Github");
 	}
 
 }
